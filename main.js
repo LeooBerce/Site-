f3efd069c6517a4771a90ff75ef0602d24caa126
LeooBerce <leonardo.berce@escola.pr.gov.br>
2026-04-29 08:29:27 -0300

    a

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function atualizaCronometro() {
 
 function comecaCronometro() {
     atualizaCronometro();
-    setInterval(atualizaCronometro, 1);
+    setInterval(atualizaCronometro, 1000);
 }
 
 comecaCronometro();
